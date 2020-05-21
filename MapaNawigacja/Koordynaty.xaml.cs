b6e9c6b315de85a41859cf715543fe7e8f1e2691
Karol Stumski <stumski.karol@gmail.com>
2020-05-21 12:13:15 +0200

    Add project files.

---
+++ MapaNawigacja/Koordynaty.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Geolocation;
+using Windows.Services.Maps;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MapaNawigacja
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Koordynaty : Page
+    {
+        public Koordynaty()
+        {
+            this.InitializeComponent();
+            GdzieJaNaMapie();
+        }
+
+
+
+        async void GdzieJaNaMapie()
+        {
+            Geolocator mojGPS = new Geolocator
+            {
+                DesiredAccuracy = PositionAccuracy.High
+            };
+            Geoposition mojeZGPS = await mojGPS.GetGeopositionAsync();
+            double dlugosc = mojeZGPS.Coordinate.Point.Position.Longitude;
+            double szerokosc = mojeZGPS.Coordinate.Point.Position.Latitude;
+            tbGPS.Text = $"długość: {dlugosc:F4} | szerokość: {szerokosc:F4}";
+            DaneGeograficzne.pktStartowy = mojeZGPS.Coordinate.Point.Position;
+            DaneGeograficzne.opisCelu = "";
+            BasicGeoposition geoposition = new BasicGeoposition
+            {
+                Latitude = dlugosc,
+                Longitude = szerokosc
+            };
+        }
+
+
+        private void goBack(object sender, RoutedEventArgs e)
+        {
+            Frame.GoBack();
+        }
+
+        private async void szukajCelu(object sender, RoutedEventArgs e)
+        {
+            DaneGeograficzne.opisCelu = txAdres.Text;
+            if (DaneGeograficzne.opisCelu == "")
+                return;
+            var wynik = await MapLocationFinder.FindLocationsAsync(DaneGeograficzne.opisCelu, new Geopoint(DaneGeograficzne.pktStartowy));
+            
+            if (wynik.Status == MapLocationFinderStatus.Success)
+            {
+                try
+                {
+                    
+                    double dlg = wynik.Locations[0].Point.Position.Longitude;
+                    double szer = wynik.Locations[0].Point.Position.Latitude;
+                    tbDlg.Text = $"{dlg:F4}";
+                    tbSzer.Text = $"{szer:F4}";
+                    DaneGeograficzne.pktDocelowy = wynik.Locations[0].Point.Position;
+                    MapRouteFinderResult routeResult = await MapRouteFinder.GetDrivingRouteAsync(
+                        new Geopoint(DaneGeograficzne.pktStartowy),
+                        new Geopoint(DaneGeograficzne.pktDocelowy),
+                        MapRouteOptimization.Time,
+                        MapRouteRestrictions.None);
+                    DaneGeograficzne.distance = (routeResult.Route.LengthInMeters / 1000).ToString();
+                }
+                catch (Exception)
+                {
+                    var dlg = new Windows.UI.Popups.MessageDialog(wynik.Status.ToString(), DaneGeograficzne.opisCelu);
+                    await dlg.ShowAsync();
+                    DaneGeograficzne.opisCelu = wynik.Status.ToString();
+                }
+            }
+            else
+            {
+                var dlg = new Windows.UI.Popups.MessageDialog(wynik.Status.ToString(), DaneGeograficzne.opisCelu);
+                await dlg.ShowAsync();
+                DaneGeograficzne.opisCelu = wynik.Status.ToString();
+            }
+        }
+    }
+}
---
+++ MapaNawigacja/MainPage.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Services.Maps;
+using Windows.UI.Xaml.Controls.Maps;
+using Windows.Media.Devices;
+using Windows.Devices.Geolocation;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MapaNawigacja
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    /// 
+
+   
+
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (DaneGeograficzne.opisCelu == null)
+                return;
+            var pkt = new Geopoint(DaneGeograficzne.pktStartowy);
+            MapIcon start = new MapIcon()
+            {
+                Location = pkt,
+                Title = "Tu jesteś"
+            };
+            MapaNawigacja.MapElements.Add(start);
+            var pktCelu = new Geopoint(DaneGeograficzne.pktDocelowy);
+            MapaNawigacja.MapElements.Add(
+                new MapIcon {
+                    Title = DaneGeograficzne.opisCelu,
+                    Location = pktCelu
+                });
+            Trasa(pkt, pktCelu);
+            MapPolyline trasaLotem = new MapPolyline
+            {
+                StrokeColor = Windows.UI.Colors.Black,
+                StrokeThickness = 3,
+                StrokeDashed = true,
+                Path = new Geopath(new List<BasicGeoposition> {DaneGeograficzne.pktStartowy,DaneGeograficzne.pktDocelowy}),
+            };
+            MapaNawigacja.MapElements.Add(trasaLotem);
+            await MapaNawigacja.TrySetViewAsync(new Geopoint(DaneGeograficzne.pktStartowy), 8);
+
+            var komunikat = new Windows.UI.Popups.MessageDialog(DaneGeograficzne.distance + " km", $"Odległość od Twojej lokalizacji do {DaneGeograficzne.opisCelu} to:");
+            await komunikat.ShowAsync();
+        }
+
+        async void Trasa(Geopoint start, Geopoint stop)
+        {
+            var wynik = await MapRouteFinder.GetDrivingRouteAsync(start, stop);
+            if (wynik.Status == MapRouteFinderStatus.Success)
+            {
+                var trasa = wynik.Route;
+                var trasaNaMape = new MapRouteView(wynik.Route)
+                {
+                    RouteColor = Windows.UI.Colors.Blue
+                };
+                MapaNawigacja.Routes.Add(trasaNaMape);
+                await MapaNawigacja.TrySetViewBoundsAsync(trasa.BoundingBox, new Thickness(25), MapAnimationKind.Bow);
+            }
+            else
+            {
+                var dlg = new Windows.UI.Popups.MessageDialog(wynik.Status.ToString(), DaneGeograficzne.opisCelu);
+                await dlg.ShowAsync();
+            }
+        }
+
+        private void powMapa(object sender, RoutedEventArgs e)
+        {
+            MapaNawigacja.ZoomLevel++;
+        }
+
+        private void pomMapa(object sender, RoutedEventArgs e)
+        {
+            MapaNawigacja.ZoomLevel--;
+        }
+
+
+        private void trybMapy(object sender, RoutedEventArgs e)
+        {
+            var ab = sender as AppBarButton;
+            FontIcon ficon = new FontIcon
+            {
+                FontFamily = FontFamily.XamlAutoFontFamily
+            };
+            FontFamily = FontFamily.XamlAutoFontFamily;
+            if (MapaNawigacja.Style == MapStyle.Road)
+            {
+                MapaNawigacja.Style = MapStyle.AerialWithRoads;
+                ficon.Glyph = "M";
+                ab.Label = "mapa";
+                ab.Icon = ficon;
+            }
+            else
+            {
+                MapaNawigacja.Style = MapStyle.Road;
+                ficon.Glyph = "S";
+                ab.Label = "satelita";
+                ab.Icon = ficon;
+            }
+        }
+
+        private void koordynaty(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Koordynaty));
+        }
+    }
+}
